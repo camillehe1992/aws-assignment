12c986ae47a8596718aa8dc3e475629a39a57ce3
He.Camille <camille.he@outlook.com>
2022-08-05 23:31:57 +0800

    chore: new RdsCdkStack

--- aws-cdk/bin/aws-cdk.ts
+++ aws-cdk/bin/aws-cdk.ts
@@ -4,11 +4,9 @@ import * as cdk from 'aws-cdk-lib';
 import { AwsCdkStack } from '../lib/aws-cdk-stack';
 import { RdsCdkStack } from '../lib/rds-cdk-stack';
 
-const app = new cdk.App();
-new AwsCdkStack(app, 'AwsCdkStack', {
-  env: { account: '824709318323', region: 'ap-south-1' },
-});
+const env = { account: '824709318323', region: 'ap-south-1' };
 
-// new RdsCdkStack(app, 'RdsCdkStack', {
-//   env: { account: '824709318323', region: 'ap-south-1' },
-// });
+const app = new cdk.App();
+
+new AwsCdkStack(app, 'AwsCdkStack', { env });
+new RdsCdkStack(app, 'RdsCdkStack', { env });
