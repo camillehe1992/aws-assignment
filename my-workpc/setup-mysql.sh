d9fce91109781a556aa64fe2e44f813467cfeba4
He.Camille <camille.he@outlook.com>
2022-08-09 23:35:20 +0800

    chore: create Dockerfile for webapp

--- my-workpc/setup-mysql.sh
+++ my-workpc/setup-mysql.sh
@@ -1,17 +1,24 @@
 #! /bin/bash
 
-# https://dev.to/yongchanghe/want-to-install-mysql-database-and-finalize-your-data-using-docker-on-your-aws-ec2-3lc6
+# https://hevodata.com/learn/docker-mysql/
 
-docker pull mysql:latest
+# pull the docker image for mysql
+docker pull mysql/mysql-server:latest
 
+# setup local files for mysql container configuration
 mkdir docker
 mkdir docker/mysql
-mkdir docker/mysql/conf
+mkdir docker/mysql/conf.d
 mkdir docker/mysql/data
 
-docker run -d mysql:latest \
+# deploy and start mysql container
+docker run \
+  -d mysql/mysql-server:latest \
   -p 3306:3306 \
-  -v /home/ubuntu/docker/mysql/conf/:/etc/mysql/conf.d \
-  -v /home/ubuntu/docker/mysql/data/:/var/lib/mysql \
+  -v /home/ubuntu/docker/mysql/conf.d:/etc/mysql/conf.d \
+  -v /home/ubuntu/docker/mysql/data:/var/lib/mysql \
   -e MYSQL_ROOT_PASSWORD=123 \
   --name docker_mysql
+
+# test mysql database connection via below cli
+# mysql --host=127.0.0.1 --port=13306 -u root -p
