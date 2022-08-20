71311a18aec32172d9546a79c013b727f74c3094
He.Camille <camille.he@outlook.com>
2022-08-20 18:39:04 +0800

    add rds init logic

--- aws-cdk/lib/rds-cdk-stack.ts
+++ aws-cdk/lib/rds-cdk-stack.ts
@@ -2,6 +2,10 @@ import * as cdk from 'aws-cdk-lib';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
+import { CdkResourceInitializer } from '../lib/resource-initializer';
+import * as cwlogs from 'aws-cdk-lib/aws-logs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
 
 interface RdsCdkStackProps extends cdk.StackProps {
   vpc: ec2.Vpc;
@@ -16,6 +20,8 @@ export class RdsCdkStack extends cdk.Stack {
     super(scope, id, props);
 
     const { vpc, securityGroup } = props;
+
+    const credentials = rds.Credentials.fromGeneratedSecret('clusteradmin');
     
     // create the rds cluster with 2 instances (1 reader and 1 writer)
     this.dbCluster = new rds.DatabaseCluster(this, 'DatabaseCluster', {
@@ -23,7 +29,7 @@ export class RdsCdkStack extends cdk.Stack {
         version: rds.AuroraMysqlEngineVersion.VER_3_02_0
       }),
       // Optional - will default to 'admin' username and generated password
-      credentials: rds.Credentials.fromGeneratedSecret('clusteradmin'),
+      credentials,
       clusterIdentifier: 'mysql',
       instanceProps: {
         // optional , defaults to t3.medium (wiil incur charge)
@@ -37,5 +43,21 @@ export class RdsCdkStack extends cdk.Stack {
         securityGroups: [securityGroup]
       },
     });
+
+    const initializer = new CdkResourceInitializer(this, 'MyRdsInit', {
+      config: {
+        credsSecretName: credentials.secretName
+      },
+      fnLogRetention: cwlogs.RetentionDays.TWO_WEEKS,
+      fnCode: lambda.DockerImageCode.fromImageAsset(`${__dirname}/rds-init-fn-code`, {}),
+      fnTimeout: cdk.Duration.minutes(2),
+      fnSecurityGroups: [],
+      vpc,
+      subnetsSelection: vpc.selectSubnets({
+        subnetType: ec2.SubnetType.PRIVATE_WITH_NAT
+      })
+    })
+    // manage resources dependency
+    initializer.customResource.node.addDependency(this.dbCluster);
   }
 }
---
+++ aws-cdk/lib/resource-initializer.ts
@@ -0,0 +1,96 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as cwlogs from 'aws-cdk-lib/aws-logs';
+import  * as iam from 'aws-cdk-lib/aws-iam'
+import { Construct } from 'constructs'
+import { AwsCustomResource, AwsCustomResourcePolicy, AwsSdkCall, PhysicalResourceId } from 'aws-cdk-lib/custom-resources'
+import { createHash } from 'crypto'
+
+export interface CdkResourceInitializerProps {
+  vpc: ec2.IVpc
+  subnetsSelection: ec2.SubnetSelection
+  fnSecurityGroups: ec2.ISecurityGroup[]
+  fnTimeout: cdk.Duration
+  fnCode: lambda.DockerImageCode
+  fnLogRetention: cwlogs.RetentionDays
+  fnMemorySize?: number
+  config: object
+}
+
+export class CdkResourceInitializer extends Construct {
+  public readonly response: string
+  public readonly customResource: AwsCustomResource
+  public readonly function: lambda.Function
+
+  constructor (scope: Construct, id: string, props: CdkResourceInitializerProps) {
+    super(scope, id);
+
+    const stack = cdk.Stack.of(this)
+
+    const fnSg = new ec2.SecurityGroup(this, 'ResourceInitializerFnSg', {
+      securityGroupName: `${id}ResourceInitializerFnSg`,
+      vpc: props.vpc,
+      allowAllOutbound: true
+    })
+
+    const fn = new lambda.DockerImageFunction(this, 'ResourceInitializerFn', {
+      memorySize: props.fnMemorySize || 128,
+      functionName: `${id}-ResInit${stack.stackName}`,
+      code: props.fnCode,
+      vpcSubnets: props.vpc.selectSubnets(props.subnetsSelection),
+      vpc: props.vpc,
+      securityGroups: [fnSg, ...props.fnSecurityGroups],
+      timeout: props.fnTimeout,
+      logRetention: props.fnLogRetention,
+      allowAllOutbound: true,
+    })
+
+    fn.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: [
+          'secretsmanager:GetSecretValue'
+        ],
+        resources: ['*'],
+        effect: iam.Effect.ALLOW
+    }))
+
+    const payload: string = JSON.stringify({
+      params: {
+        config: props.config
+      }
+    })
+
+    const payloadHashPrefix = createHash('md5').update(payload).digest('hex').substring(0, 6)
+
+    const sdkCall: AwsSdkCall = {
+      service: 'Lambda',
+      action: 'invoke',
+      parameters: {
+        FunctionName: fn.functionName,
+        Payload: payload
+      },
+      physicalResourceId: PhysicalResourceId.of(`${id}-AwsSdkCall-${fn.currentVersion.version + payloadHashPrefix}`)
+    }
+    
+    const customResourceFnRole = new iam.Role(this, 'AwsCustomResourceRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')
+    })
+    customResourceFnRole.addToPolicy(
+      new iam.PolicyStatement({
+        resources: [`arn:aws:lambda:${stack.region}:${stack.account}:function:*-ResInit${stack.stackName}`],
+        actions: ['lambda:InvokeFunction']
+      })
+    )
+    this.customResource = new AwsCustomResource(this, 'AwsCustomResource', {
+      policy: AwsCustomResourcePolicy.fromSdkCalls({ resources: AwsCustomResourcePolicy.ANY_RESOURCE }),
+      onUpdate: sdkCall,
+      timeout: cdk.Duration.minutes(10),
+      role: customResourceFnRole
+    })
+
+    this.response = this.customResource.getResponseField('Payload')
+
+    this.function = fn
+  }
+}
